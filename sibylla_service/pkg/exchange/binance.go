221632b7b950111a213edfb13d2b9fab895cd2ab
cdrn <chrisdoran@protonmail.com>
2024-10-29 21:03:04 +1100

    fix: reset ws connection every hour, flashing lights

--- sibylla_service/pkg/exchange/binance.go
+++ sibylla_service/pkg/exchange/binance.go
@@ -15,84 +15,90 @@ import (
 )
 
 func ConnectBinanceWebSocket(config exchangeconfig.Config) {
-	// Create a channel to receive OS signals
-	interrupt := make(chan os.Signal, 1)
-	// Notify the interrupt channel on receiving an interrupt signal
-	signal.Notify(interrupt, os.Interrupt)
-
-	// Define the WebSocket URL for Binance
-	// TODO: env var
-	u := url.URL{Scheme: "wss", Host: "stream.binance.com:9443", Path: "/ws/btcusdt@trade"}
-	log.Printf("connecting to %s", u.String())
-
-	// Connect to the WebSocket server
-	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
-	if err != nil {
-		log.Fatal("dial:", err)
-	}
-	defer c.Close() // Ensure the connection is closed when the function exits
-
-	// Create a channel to signal when the connection is done
-	done := make(chan struct{})
-
-	// Start a goroutine to read messages from the WebSocket
-	go func() {
-		// Defer executes after function completion. close socket.
-		defer close(done)
-		for {
-			_, message, err := c.ReadMessage()
-			if err != nil {
-				log.Println("read:", err)
-				return
-			}
-
-			// Unpack the trade message into the BinanceTrade struct
-			var binanceTrade trade.BinanceTrade
-			err = json.Unmarshal(message, &binanceTrade)
-			if err != nil {
-				log.Printf("Could not unmarshal trade message: %v", err)
-				continue
-			}
-
-			// Map BinanceTrade to the Trade struct
-			tradeData := trade.Trade{
-				Exchange:     "binance",
-				Pair:         binanceTrade.Symbol,
-				Price:        func() float64 { p, _ := strconv.ParseFloat(binanceTrade.Price, 64); return p }(),
-				Quantity:     func() float64 { q, _ := strconv.ParseFloat(binanceTrade.Quantity, 64); return q }(),
-				Timestamp:    binanceTrade.TradeTime,
-				IsBuyerMaker: binanceTrade.IsBuyerMaker,
-			}
-
-			// Push the trade struct into redis
-			err = config.RedisClient.PushToList("trades:binance:BTC/USDT", tradeData, 100)
-			if err != nil {
-				log.Printf("Could not push trade to Redis: %v", err)
-			} else {
-				log.Printf("Binance trade pushed to redis")
-			}
-		}
-	}()
-
 	for {
-		select {
-		case <-done: // Exit the loop on done sig
-			return
-		case <-interrupt: // If an interrupt signal is received
-			log.Println("interrupt")
+		// Create a channel to receive OS signals
+		interrupt := make(chan os.Signal, 1)
+		// Notify the interrupt channel on receiving an interrupt signal
+		signal.Notify(interrupt, os.Interrupt)
 
-			// Cleanly close the connection by sending a close message and then
-			// waiting (with timeout) for the server to close the connection.
-			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-			if err != nil {
-				log.Println("write close:", err)
+		// Define the WebSocket URL for Binance
+		// TODO: env var
+		u := url.URL{Scheme: "wss", Host: "stream.binance.com:9443", Path: "/ws/btcusdt@trade"}
+		log.Printf("connecting to %s", u.String())
+
+		// Connect to the WebSocket server
+		c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+		if err != nil {
+			log.Fatal("dial:", err)
+		}
+		defer c.Close() // Ensure the connection is closed when the function exits
+
+		// Create a channel to signal when the connection is done
+		done := make(chan struct{})
+
+		// Start a goroutine to read messages from the WebSocket
+		go func() {
+			// Defer executes after function completion. close socket.
+			defer close(done)
+			for {
+				_, message, err := c.ReadMessage()
+				if err != nil {
+					log.Println("read:", err)
+					return
+				}
+
+				// Unpack the trade message into the BinanceTrade struct
+				var binanceTrade trade.BinanceTrade
+				err = json.Unmarshal(message, &binanceTrade)
+				if err != nil {
+					log.Printf("Could not unmarshal trade message: %v", err)
+					continue
+				}
+
+				// Map BinanceTrade to the Trade struct
+				tradeData := trade.Trade{
+					Exchange:     "binance",
+					Pair:         binanceTrade.Symbol,
+					Price:        func() float64 { p, _ := strconv.ParseFloat(binanceTrade.Price, 64); return p }(),
+					Quantity:     func() float64 { q, _ := strconv.ParseFloat(binanceTrade.Quantity, 64); return q }(),
+					Timestamp:    binanceTrade.TradeTime,
+					IsBuyerMaker: binanceTrade.IsBuyerMaker,
+				}
+
+				// Push the trade struct into redis
+				err = config.RedisClient.PushToList("trades:binance:BTC/USDT", tradeData, 100)
+				if err != nil {
+					log.Printf("Could not push trade to Redis: %v", err)
+				} else {
+					log.Printf("Binance trade pushed to redis")
+				}
+			}
+		}()
+
+		for {
+			select {
+			case <-done: // Exit the loop on done sig
+				return
+			case <-interrupt: // If an interrupt signal is received
+				log.Println("interrupt")
+
+				// Cleanly close the connection by sending a close message and then
+				// waiting (with timeout) for the server to close the connection.
+				err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+				if err != nil {
+					log.Println("write close:", err)
+					return
+				}
+				select {
+				case <-done: // Wait for the done channel to be closed
+				case <-time.After(time.Second): // Or timeout after 1 second
+				}
+				return
+			case <-time.After(time.Hour): // Reset the connection every hour
+				log.Println("Resetting WebSocket connection for Binance")
+				c.Close()
 				return
 			}
-			select {
-			case <-done: // Wait for the done channel to be closed
-			case <-time.After(time.Second): // Or timeout after 1 second
-			}
-			return
 		}
 	}
 }
--- sibylla_service/pkg/exchange/kraken.go
+++ sibylla_service/pkg/exchange/kraken.go
@@ -15,103 +15,109 @@ import (
 )
 
 func ConnectKrakenWebSocket(config exchangeconfig.Config) {
-	// Create a channel to receive OS signals
-	interrupt := make(chan os.Signal, 1)
-	// Notify the interrupt channel on receiving an interrupt signal
-	signal.Notify(interrupt, os.Interrupt)
-
-	// Define the WebSocket URL for Kraken
-	u := url.URL{Scheme: "wss", Host: "ws.kraken.com", Path: "/v2"}
-	log.Printf("connecting to %s", u.String())
-
-	// Connect to the WebSocket server
-	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
-	if err != nil {
-		log.Fatal("dial:", err)
-	}
-	defer c.Close() // Ensure the connection is closed when the function exits
-
-	// Create a channel to signal when the connection is done
-	done := make(chan struct{})
-
-	// Subscribe to the trade channel for MATIC/USD
-	subscribeMessage := map[string]interface{}{
-		"method": "subscribe",
-		"params": map[string]interface{}{
-			"channel":  "trade",
-			"symbol":   []string{"BTC/USD"},
-			"snapshot": false,
-		},
-	}
-	subscribeMessageJSON, err := json.Marshal(subscribeMessage)
-	if err != nil {
-		log.Fatal("subscribe message marshal:", err)
-	}
-
-	err = c.WriteMessage(websocket.TextMessage, subscribeMessageJSON)
-	if err != nil {
-		log.Fatal("subscribe message send:", err)
-	}
-
-	// Start a goroutine to read messages from the WebSocket
-	go func() {
-		// Defer executes after function completion. close socket.
-		defer close(done)
-		for {
-			_, message, err := c.ReadMessage()
-			if err != nil {
-				log.Println("read:", err)
-				return
-			}
-
-			// Unpack the trade message into the KrakenTrade struct
-			var krakenTrade trade.KrakenTradeMessage
-			err = json.Unmarshal(message, &krakenTrade)
-			if err != nil {
-				log.Printf("Could not unmarshal trade message: %v", err)
-				continue
-			}
-			for _, tradeData := range krakenTrade.Data {
-				// Map KrakenTradeMessage data to the Trade struct
-				trade := trade.Trade{
-					Exchange:     "kraken",
-					Pair:         tradeData.Symbol,
-					Price:        tradeData.Price,
-					Quantity:     tradeData.Quantity,
-					Timestamp:    func() int64 { t, _ := strconv.ParseInt(tradeData.Timestamp, 10, 64); return t }(),
-					IsBuyerMaker: tradeData.Side == "sell",
-				}
-
-				// Push the trade struct into redis
-				err = config.RedisClient.PushToList("trades:kraken:BTC/USD", trade, 100)
-				if err != nil {
-					log.Printf("Could not push trade to Redis: %v", err)
-				} else {
-					log.Printf("Kraken trade added to redis")
-				}
-			}
-		}
-	}()
-
 	for {
-		select {
-		case <-done: // Exit the loop on done sig
-			return
-		case <-interrupt: // If an interrupt signal is received
-			log.Println("interrupt")
+		// Create a channel to receive OS signals
+		interrupt := make(chan os.Signal, 1)
+		// Notify the interrupt channel on receiving an interrupt signal
+		signal.Notify(interrupt, os.Interrupt)
 
-			// Cleanly close the connection by sending a close message and then
-			// waiting (with timeout) for the server to close the connection.
-			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-			if err != nil {
-				log.Println("write close:", err)
+		// Define the WebSocket URL for Kraken
+		u := url.URL{Scheme: "wss", Host: "ws.kraken.com", Path: "/v2"}
+		log.Printf("connecting to %s", u.String())
+
+		// Connect to the WebSocket server
+		c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+		if err != nil {
+			log.Fatal("dial:", err)
+		}
+		defer c.Close() // Ensure the connection is closed when the function exits
+
+		// Create a channel to signal when the connection is done
+		done := make(chan struct{})
+
+		// Subscribe to the trade channel for MATIC/USD
+		subscribeMessage := map[string]interface{}{
+			"method": "subscribe",
+			"params": map[string]interface{}{
+				"channel":  "trade",
+				"symbol":   []string{"BTC/USD"},
+				"snapshot": false,
+			},
+		}
+		subscribeMessageJSON, err := json.Marshal(subscribeMessage)
+		if err != nil {
+			log.Fatal("subscribe message marshal:", err)
+		}
+
+		err = c.WriteMessage(websocket.TextMessage, subscribeMessageJSON)
+		if err != nil {
+			log.Fatal("subscribe message send:", err)
+		}
+
+		// Start a goroutine to read messages from the WebSocket
+		go func() {
+			// Defer executes after function completion. close socket.
+			defer close(done)
+			for {
+				_, message, err := c.ReadMessage()
+				if err != nil {
+					log.Println("read:", err)
+					return
+				}
+
+				// Unpack the trade message into the KrakenTrade struct
+				var krakenTrade trade.KrakenTradeMessage
+				err = json.Unmarshal(message, &krakenTrade)
+				if err != nil {
+					log.Printf("Could not unmarshal trade message: %v", err)
+					continue
+				}
+				for _, tradeData := range krakenTrade.Data {
+					// Map KrakenTradeMessage data to the Trade struct
+					trade := trade.Trade{
+						Exchange:     "kraken",
+						Pair:         tradeData.Symbol,
+						Price:        tradeData.Price,
+						Quantity:     tradeData.Quantity,
+						Timestamp:    func() int64 { t, _ := strconv.ParseInt(tradeData.Timestamp, 10, 64); return t }(),
+						IsBuyerMaker: tradeData.Side == "sell",
+					}
+
+					// Push the trade struct into redis
+					err = config.RedisClient.PushToList("trades:kraken:BTC/USD", trade, 100)
+					if err != nil {
+						log.Printf("Could not push trade to Redis: %v", err)
+					} else {
+						log.Printf("Kraken trade added to redis")
+					}
+				}
+			}
+		}()
+
+		for {
+			select {
+			case <-done: // Exit the loop on done sig
+				return
+			case <-interrupt: // If an interrupt signal is received
+				log.Println("interrupt")
+
+				// Cleanly close the connection by sending a close message and then
+				// waiting (with timeout) for the server to close the connection.
+				err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+				if err != nil {
+					log.Println("write close:", err)
+					return
+				}
+				select {
+				case <-done: // Wait for the done channel to be closed
+				case <-time.After(time.Second): // Or timeout after 1 second
+				}
+				return
+			case <-time.After(time.Hour): // Reset the connection every hour
+				log.Println("Resetting WebSocket connection for Kraken")
+				c.Close()
 				return
 			}
-			select {
-			case <-done: // Wait for the done channel to be closed
-			case <-time.After(time.Second): // Or timeout after 1 second
-			}
-			return
 		}
 	}
 }
